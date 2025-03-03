b21ab274e2601bdd8c927be9a9a79431b82e2975
Gelson <gelsonblu@gmail.com>
2025-03-02 21:40:13 -0300

    parametros melhorados

--- modulo_3/desafio/hiperparametr_indian_dataset_vc_random_search.py
+++ modulo_3/desafio/hiperparametr_indian_dataset_vc_random_search.py
@@ -27,49 +27,27 @@ print('Amostras e Features:', df.shape)
 X = df.drop('Class', axis=1)
 y = df['Class']
 
-#definindo hiperparâmetros
-hiperparam = {'kernel':('sigmoid', 'rbf'), 'C':[0.01, 1, 10]}
-#definindo o tipo de validacao cruzada e o numero de folds
 #definindo o tipo de validacao cruzada e o numero de folds
 cv_strat = StratifiedKFold(n_splits = 10)
 #instânciando meu classificador
 classifier = SVC()
 #definindo a estrategia de score a partir da metrica f1
 f1 = make_scorer(f1_score)
-#instanciando e modelando o grid search com os hiperparametros e a validação definidas.
-grid_cv = GridSearchCV(classifier, hiperparam, cv = cv_strat, scoring = f1)
-#definindo a estrategia de score a partir da metrica f1
-f1 = make_scorer(f1_score)
-
 #definindo hiperparâmetros
 distributions = dict(kernel = ['sigmoid', 'rbf'], C = uniform(loc=0, scale=10))
-
-#instânciando meu classificador
-classifier = SVC()
-
 #instanciando e modelando o grid search com os hiperparametros e a validação definidas.
 random_cv = RandomizedSearchCV(classifier, distributions, cv = cv_strat, scoring = f1, random_state = 42, n_iter = 10)
 random_cv.fit(X, y)
-#vamos olhar para todos os resultados encontrados!
 print('Resumo de todos os resultados encontrados:\n\n', random_cv.cv_results_)
-
-#vamos olhar para os melhores resultados encontrados pelo Grid Search
 print('Melhor resultado f1:', random_cv.best_score_)
 print('\n\nMelhor configuração de hiperparâmetros:', random_cv.best_params_)
 print( '\n\nConfigurações de todos os hiperparâmetros do melhor estimado encontrado pelo GridSearch: \n', random_cv.best_estimator_)
 
 print('----------- Vamos testar agora o mesmo processo para o RF ------')
-#definindo o tipo de validacao cruzada e o numero de folds
-cv_strat = StratifiedKFold(n_splits = 10)
-
-#definindo a estrategia de score a partir da metrica f1
-f1 = make_scorer(f1_score)
-
 #definindo hiperparâmetros
 distributions1 = dict(n_estimators = randint(10, 100),
                       bootstrap = [True, False],
                       criterion = ['gini', 'entropy'])
-
 #instânciando meu classificador
 classifier1 = RandomForestClassifier(random_state = 42)
 
