12def6b8b3680bfb923eb398f3f88232b0b59079
Gelson <gelsonblu@gmail.com>
2025-04-22 00:17:56 -0300

    graficos

--- arq/modulo_4/teste_final/verify_dataset.py
+++ arq/modulo_4/teste_final/verify_dataset.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 
@@ -37,9 +40,40 @@ print(df.info())
 print(df.head())
 print(df.describe())
 print(df.isnull().sum())
-acumulados = df.groupby("ACTIVITY").count()['SL']
-print(acumulados)
+d = df["ACTIVITY"].value_counts().sort_index()
+print(d)
+# Pie chart, where the slices will be ordered and plotted counter-clockwise:
+dict = {0:'Em pé', 1:'Andando', 2:'Sentado', 3:'Caindo', 4:'Cãibras', 5:'Correndo'}
+resp = list(dict.keys())
+labels = list(dict.values())
+sizes = [d[0], d[1], d[2], d[3], d[4], d[5]]
+explode = (0, 0, 0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, labels=labels, explode = explode, autopct='%1.1f%%', startangle = 90, counterclock=False, shadow=False)
+ax1.axis('equal')
+plt.show()
+
+
 
 # df_teste = df.replace({'ACTIVITY':{0:'Standing',1:'Walking',2:'Sitting',3:'Falling',4:'Cramps',5:'Running'}})
 # print(df_teste.head())
-print(df.shape)
+# print(df.shape)
+# print(df.shape)
+# df.boxplot(column=['SL', 'EEG', 'BP', 'HR'])
+# plt.show()
+# df.hist(bins=20, figsize=(20,20), color='g')
+Target = df['ACTIVITY']
+Features = df[['TIME','SL','EEG','BP','HR','CIRCLUATION']]
+
+X_train, X_test, y_train, y_test = train_test_split(Features, Target, test_size = 0.3)
+
+rfc = RandomForestClassifier(
+    n_estimators = 50,
+    #min_samples_leaf=10,
+    #min_samples_split=10
+)
+rfc.fit(X_train, y_train)
+predictions = rfc.predict(X_test)
+accuracy = accuracy_score(y_test, predictions)
+print(accuracy)
