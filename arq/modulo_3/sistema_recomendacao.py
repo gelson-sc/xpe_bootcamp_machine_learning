d63ea01109a948dc83358ea702286ea0323cfdc5
Gelson <gelsonblu@gmail.com>
2025-04-23 01:24:28 -0300

    funcoes

--- arq/modulo_3/sistema_recomendacao.py
+++ arq/modulo_3/sistema_recomendacao.py
@@ -15,5 +15,24 @@ y = np.array([5, 5])
 d = euclidean_distance(x, y)
 print('a distancia euclidiana de x e y:', d, type(d))
 # calculo normas
-nrm = np.linalg.norm(x-y)
+nrm = np.linalg.norm(x - y)
 print(nrm, type(nrm))
+
+
+def hamming_distance(x: np.array, y: np.array, normalize=False):
+    factor = 1
+    if normalize:
+        factor = 1 / len(x)
+    distance = factor * np.sum(np.abs(x != y))
+    return distance
+
+
+x = np.array([1, 0, 0, 1, 1, 0])
+y = np.array([1, 1, 0, 0, 0, 0])
+h = hamming_distance(x, y)
+print('distancia de hamming:', h, type(h))
+hn = hamming_distance(x, y, True)
+print('distancia de hamming normalizada:', hn, type(hn))
+# funcao pacote scipy
+hnn = hamming(x, y)
+print('distancia de hamming normalizada f:', hnn, type(hnn))
